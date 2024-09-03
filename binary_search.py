3803d79898c88ec4eae00f600ce11dc59819e3d9
Kwame Owusu <98961379+kwame-Owusu@users.noreply.github.com>
2024-09-03 14:07:10 +0100

    Create binary_search.py
    this is an implementation of binary search

---
+++ binary_search.py
@@ -0,0 +1,17 @@
+def binary_search(arr: list[int], target: int) -> int:
+    l, r = 0, len(arr) - 1
+
+    while l <=  r:
+        mid = l + (r - l) // 2 
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            l = mid + 1
+        else:
+            r = mid - 1
+    return -1
+
+#test cases
+test_arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+test_target1 = 6
+print(binary_search(test_arr1,test_target1)) # output 5
