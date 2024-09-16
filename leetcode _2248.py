7e49c947a71b675b4b7ef1ea2cf85cb63403795a
Kwame Owusu <98961379+kwame-Owusu@users.noreply.github.com>
2024-09-16 10:38:10 +0100

    Create leetcode _2248.py
    solution for leetcode 2248 2248. Intersection of Multiple Arrays

---
+++ leetcode _2248.py
@@ -0,0 +1,25 @@
+"""
+Given a 2D integer array nums where nums[i] is a non-empty array of distinct positive integers, return the list of integers that are present in each array of nums sorted in ascending order.
+Example 1:
+
+Input: nums = [[3,1,2,4,5],[1,2,3,4],[3,4,5,6]]
+Output: [3,4]
+Explanation: 
+The only integers present in each of nums[0] = [3,1,2,4,5], nums[1] = [1,2,3,4], and nums[2] = [3,4,5,6] are 3 and 4, so we return [3,4].
+"""
+
+hashmap = {}
+result = []
+
+for i in range(len(nums)):
+  for j in nums[i]:
+    if j in hashmap:
+      hashmap[j] += 1
+    else:
+      hashmap[j] = 1
+
+for key, value in hashmap.items():
+  if value == len(nums):
+    result.append(key)
+
+return sorted(result)
