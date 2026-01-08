0e43864138d088c6557b137f5252f6acefb0d578
kwame-Owusu <alfredowusu595@gmail.com>
2026-01-08 20:51:19 +0000

    function implementation

--- golang/remove-edge-vowels/main.go
+++ golang/remove-edge-vowels/main.go
@@ -1,11 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	fmt.Println(removeEdgeVowels("solving remove edge vowels"))
+	fmt.Println(removeEdgeVowels("hello world, i love programming"))
+	fmt.Println(removeEdgeVowels("hi, how are you doing"))
 }
 
 func removeEdgeVowels(s string) string {
-	return s
+	vowels := "aeiouAEIOU"
+	words := strings.Split(s, " ")
+
+	for i, word := range words {
+		runes := []rune(word)
+
+		firstVowel := -1
+		lastVowel := -1
+
+		// find first and last vowel positions
+		for j, r := range runes {
+			if strings.ContainsRune(vowels, r) {
+				if firstVowel == -1 {
+					firstVowel = j
+				}
+				lastVowel = j
+			}
+		}
+
+		// no vowels, leave word unchanged
+		if firstVowel == -1 {
+			continue
+		}
+
+		// build new word
+		var builder []rune
+		for j, r := range runes {
+			if j != firstVowel && j != lastVowel {
+				builder = append(builder, r)
+			}
+		}
+
+		words[i] = string(builder)
+	}
+
+	return strings.Join(words, " ")
 }
